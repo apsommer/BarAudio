157a84a305616d8b87a8fabd512fb88eaceb34ea
Drew Sommer <drew@sommerengineering.com>
2024-10-05 20:35:43 +0300

    style: Stub login screen ui

--- app/src/main/java/com/sommerengineering/baraudio/login/LoginScreen.kt
+++ app/src/main/java/com/sommerengineering/baraudio/login/LoginScreen.kt
@@ -1,7 +1,46 @@
 package com.sommerengineering.baraudio.login
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 sealed class LoginState {
     object Loading : LoginState()
     data class Success(val account: String) : LoginState()
     data class Error(val message: String) : LoginState()
+}
+
+@Composable
+fun LoginScreen (
+    modifier: Modifier = Modifier) {
+
+    // todo get state
+
+    Surface(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = { },
+                modifier = Modifier.size(
+                    width = 120.dp,
+                    height = 60.dp
+                )
+            ) {
+                Text(
+                    text = "Sign in with Google ..."
+                )
+            }
+        }
+    }
+
 }
