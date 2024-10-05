d74f2d5803b6fb3f3df7b7abec0119afc89abb92
Drew Sommer <drew@sommerengineering.com>
2024-10-05 20:58:35 +0300

    style: Continue login screen layout

--- app/src/main/java/com/sommerengineering/baraudio/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/baraudio/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sommerengineering.baraudio.login.LoginScreen
 import com.sommerengineering.baraudio.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LoginScreen(innerPadding)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/sommerengineering/baraudio/login/LoginScreen.kt
+++ app/src/main/java/com/sommerengineering/baraudio/login/LoginScreen.kt
@@ -1,15 +1,24 @@
 package com.sommerengineering.baraudio.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.sommerengineering.baraudio.R
 
 sealed class LoginState {
     object Loading : LoginState()
@@ -19,25 +28,38 @@ sealed class LoginState {
 
 @Composable
 fun LoginScreen (
+    innerPadding: PaddingValues,
     modifier: Modifier = Modifier) {
 
     // todo get state
 
-    Surface(
-        modifier = modifier.fillMaxSize()
-    ) {
+    Surface {
         Column(
-            verticalArrangement = Arrangement.Center
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .fillMaxSize()
+                .padding(60.dp)
         ) {
+            Image(
+                painterResource(id = R.drawable.logo_full),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.height(60.dp))
             Button(
                 onClick = { },
-                modifier = Modifier.size(
-                    width = 120.dp,
-                    height = 60.dp
-                )
+                modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
                     text = "Sign in with Google ..."
+                )
+            }
+            Spacer(modifier = Modifier.height(12.dp))
+            Button(
+                onClick = { },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Sign in with Github ..."
                 )
             }
         }
