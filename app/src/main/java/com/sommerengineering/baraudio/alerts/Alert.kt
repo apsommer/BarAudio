1deca3ed9a36d8c9ca2f1c03029c803a0ec0be87
Drew Sommer <drew@sommerengineering.com>
2024-10-10 00:20:26 +0300

    feat: Stub alert model

---
+++ app/src/main/java/com/sommerengineering/baraudio/alerts/Alert.kt
@@ -0,0 +1,11 @@
+package com.sommerengineering.baraudio.alerts
+
+class Alert(
+    var name: String,
+    var sound: Int,
+    var voice: String,
+    var speed: Float,
+    var queueBehavior: String,
+    var webhook: String
+)
+
