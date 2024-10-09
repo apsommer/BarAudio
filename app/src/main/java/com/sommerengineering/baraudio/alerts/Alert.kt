621af991bd093e4476a88a49eafa91655988d363
Drew Sommer <drew@sommerengineering.com>
2024-10-10 00:33:05 +0300

    feat: Stub example data

--- app/src/main/java/com/sommerengineering/baraudio/alerts/Alert.kt
+++ app/src/main/java/com/sommerengineering/baraudio/alerts/Alert.kt
@@ -5,7 +5,22 @@ class Alert(
     var sound: Int,
     var voice: String,
     var speed: Float,
-    var queueBehavior: String,
+    var queueBehavior: QueueBehavior,
     var webhook: String
-)
+) {
 
+}
+
+sealed class QueueBehavior {
+    object AddToQueue : QueueBehavior()
+    object ReplaceQueue : QueueBehavior()
+}
+
+fun getAlerts() : List<Alert> {
+    return listOf(
+        Alert("NQ", 1, "English", 1.0f, QueueBehavior.AddToQueue, "https://webhook.123"),
+        Alert("ES", 2, "Spanish", 1.1f, QueueBehavior.ReplaceQueue, "https://webhook.234"),
+        Alert("RTY", 3, "Swedish", 1.2f, QueueBehavior.AddToQueue, "https://webhook.345"),
+        Alert("FESX", 4, "Catalan", 1.3f, QueueBehavior.ReplaceQueue, "https://webhook.456")
+    )
+}
