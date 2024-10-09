131e4dc1daac6e1411b6b5c7c52253723c7dbaa4
Drew Sommer <drew@sommerengineering.com>
2024-10-09 13:13:01 +0300

    version: Add navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,10 @@ android {
 
 dependencies {
 
+    // version
+    val nav_version = "2.8.2"
+
+    // default october 2024
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -66,4 +70,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(kotlin("script-runtime"))
+
+    // navigation
+    implementation("androidx.navigation:navigation-compose:$nav_version")
 }
