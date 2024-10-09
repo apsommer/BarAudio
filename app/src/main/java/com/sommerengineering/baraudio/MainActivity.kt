9debe4e85f720949effb02905ec4afb61ae94602
Drew Sommer <drew@sommerengineering.com>
2024-10-09 12:36:57 +0300

    chore: Preview ui in ide

--- app/src/main/java/com/sommerengineering/baraudio/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/baraudio/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.sommerengineering.baraudio
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -20,10 +22,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LoginScreen(innerPadding)
-                }
+                LoginScreen()
             }
         }
+    }
+}
+
+@Preview(
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "dark"
+)
+@Preview(
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "light"
+)
+@Composable
+fun PreviewDarkLight() {
+    AppTheme {
+        LoginScreen()
     }
 }
--- app/src/main/java/com/sommerengineering/baraudio/login/LoginScreen.kt
+++ app/src/main/java/com/sommerengineering/baraudio/login/LoginScreen.kt
@@ -1,7 +1,8 @@
 package com.sommerengineering.baraudio.login
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -9,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,6 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.sommerengineering.baraudio.R
 
@@ -28,7 +30,6 @@ sealed class LoginState {
 
 @Composable
 fun LoginScreen (
-    innerPadding: PaddingValues,
     modifier: Modifier = Modifier) {
 
     // todo get state
@@ -64,5 +65,4 @@ fun LoginScreen (
             }
         }
     }
-
 }
