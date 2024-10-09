220497d8d4d6e0520e87d1469dc3f47073ed205e
Drew Sommer <drew@sommerengineering.com>
2024-10-09 13:07:40 +0300

    chore: Preview alerts ui in ide

--- app/src/main/java/com/sommerengineering/baraudio/alerts/AlertsScreen.kt
+++ app/src/main/java/com/sommerengineering/baraudio/alerts/AlertsScreen.kt
@@ -1,10 +1,15 @@
 package com.sommerengineering.baraudio.alerts
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sommerengineering.baraudio.login.LoginScreen
+import com.sommerengineering.baraudio.ui.theme.AppTheme
 
 @Composable
 fun AlertsScreen(
@@ -19,5 +24,20 @@ fun AlertsScreen(
         LazyColumn(Modifier.padding(padding)) {
             // todo add alerts
         }
+    }
+}
+
+@Preview(
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "dark"
+)
+@Preview(
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "light"
+)
+@Composable
+fun PreviewDarkLight() {
+    AppTheme {
+        AlertsScreen()
     }
 }
