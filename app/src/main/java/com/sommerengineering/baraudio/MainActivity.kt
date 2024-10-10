0e7c9bbfb0d3bfba8536ee4c21c53dee98f3aecd
Drew Sommer <drew@sommerengineering.com>
2024-10-10 11:25:49 +0300

    refactor: Isolate navigation component

--- app/src/main/java/com/sommerengineering/baraudio/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/baraudio/MainActivity.kt
@@ -1,26 +1,14 @@
 package com.sommerengineering.baraudio
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.sommerengineering.baraudio.alerts.AlertsScreen
-import com.sommerengineering.baraudio.alerts.AlertsState
-import com.sommerengineering.baraudio.login.LoginScreen
-import com.sommerengineering.baraudio.login.LoginState
 import com.sommerengineering.baraudio.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,35 +29,8 @@ fun App() {
         val navController = rememberNavController()
         
         Scaffold { padding ->
-            NavigationHost(navController)
+            Navigation(navController)
             Modifier.padding(padding)
         }
     }
-}
-
-@Composable
-fun NavigationHost(
-    controller: NavHostController,
-    modifier: Modifier = Modifier) {
-
-    // host is container for current destination
-    NavHost(
-        navController = controller,
-        startDestination = LoginState.route,
-        modifier = modifier
-    ) {
-        composable(
-            route = LoginState.route) {
-            LoginScreen(
-                onClickLoginWithGoogle = {
-                    controller.navigate(AlertsState.route)
-                }
-            )
-        }
-        composable(
-            route = AlertsState.route) {
-            AlertsScreen()
-        }
-    }
-
 }
---
+++ app/src/main/java/com/sommerengineering/baraudio/Navigation.kt
@@ -0,0 +1,39 @@
+package com.sommerengineering.baraudio
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.sommerengineering.baraudio.alerts.AlertsScreen
+import com.sommerengineering.baraudio.alerts.AlertsState
+import com.sommerengineering.baraudio.login.LoginScreen
+import com.sommerengineering.baraudio.login.LoginState
+
+@Composable
+fun Navigation(
+    controller: NavHostController,
+    modifier: Modifier = Modifier
+) {
+
+    // host is container for current destination
+    NavHost(
+        navController = controller,
+        startDestination = LoginState.route,
+        modifier = modifier
+    ) {
+        composable(
+            route = LoginState.route) {
+            LoginScreen(
+                onClickLoginWithGoogle = {
+                    controller.navigate(AlertsState.route)
+                }
+            )
+        }
+        composable(
+            route = AlertsState.route) {
+            AlertsScreen()
+        }
+    }
+
+}
