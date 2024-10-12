377f18938c3329b251824bf06842a7ec7b9e6f69
Drew Sommer <drew@sommerengineering.com>
2024-10-12 12:25:01 +0300

    feat: Build sign request and handle response

--- app/src/main/java/com/sommerengineering/baraudio/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/baraudio/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.sommerengineering.baraudio
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,7 +10,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import com.sommerengineering.baraudio.login.LoginScreen
 import com.sommerengineering.baraudio.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,4 +37,19 @@ fun App() {
             Modifier.padding(padding)
         }
     }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+@Preview(
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "dark"
+)
+@Preview(
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "light"
+)
+@Composable
+fun PreviewApp() {
+    App()
 }
--- app/src/main/java/com/sommerengineering/baraudio/login/GoogleAuthenticationProvider.kt
+++ app/src/main/java/com/sommerengineering/baraudio/login/GoogleAuthenticationProvider.kt
@@ -1,14 +1,24 @@
 package com.sommerengineering.baraudio.login
 
 import android.content.Context
+import android.credentials.GetCredentialException
+import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class GoogleAuthenticationProvider (
-    private val activityContext: Context
+fun googleSignIn (
+    activityContext: Context
 ) {
 
-    // instantiate google sign-in request
+    // create credential manager and coroutine
+    val credentialManager = CredentialManager.create(activityContext)
+    val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    // consider google accounts on device
     val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
         .setFilterByAuthorizedAccounts(true) // filter by accounts already used to sign-in
         .setAutoSelectEnabled(true) // automatic sign-in for users who register with their single account
@@ -16,9 +26,30 @@ class GoogleAuthenticationProvider (
 
     // todo sign-up with setFilterByAuthorizedAccounts(false)
 
+    // create request
     val request: GetCredentialRequest = GetCredentialRequest.Builder()
         .addCredentialOption(googleIdOption)
         .build()
 
+    coroutineScope.launch {
+        try {
+            val result = credentialManager.getCredential(
+                request = request,
+                context = activityContext
+            )
+            handleSuccess(result)
+        } catch (exception: Exception) {
+            handleFailure(exception)
+        }
+    }
+
     // todo sign-out with clearCredentialState()
+}
+
+fun handleSuccess(result: GetCredentialResponse) {
+
+}
+
+fun handleFailure(exception: Exception) {
+
 }
