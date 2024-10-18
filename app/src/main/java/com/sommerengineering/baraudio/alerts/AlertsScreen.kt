a931ee2ebd3ea6f41b948ffbd86108c16f5a76b0
Drew Sommer <drew@sommerengineering.com>
2024-10-18 22:10:39 +0300

    refactor: Simplify persmission request flow

--- app/src/main/java/com/sommerengineering/baraudio/alerts/AlertsScreen.kt
+++ app/src/main/java/com/sommerengineering/baraudio/alerts/AlertsScreen.kt
@@ -2,27 +2,16 @@ package com.sommerengineering.baraudio.alerts
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.sommerengineering.baraudio.MainActivity
-import com.sommerengineering.baraudio.R
 import com.sommerengineering.baraudio.theme.AppTheme
 
 sealed class AlertsState {
