53615d531a4ca4f34790e2d526274b73fc0b65fe
Drew Sommer <drew@sommerengineering.com>
2024-10-24 22:07:32 +0300

    test: Extract json from request

--- functions/main.py
+++ functions/main.py
@@ -1,11 +1,14 @@
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
+from flask import request, json
 
 # initialize
 initialize_app()
 
 # define function endpoint
 @https_fn.on_request()
-def baraudio(req: https_fn.Request) -> https_fn.Response:
-    response = "Python is better than JavaScript"
+def baraudio(request: https_fn.Request) -> https_fn.Response:
+    requestJson = request.json
+    jsonString = json.dumps(requestJson, default = str)
+    response = jsonString
     return https_fn.Response(response)
