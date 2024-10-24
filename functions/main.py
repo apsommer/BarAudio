369e03d8af2366abe8d8337bf91173a8bd12fba7
Drew Sommer <drew@sommerengineering.com>
2024-10-24 19:31:48 +0300

    chore: Remove local code from repository

--- functions/main.py
+++ functions/main.py
@@ -1,12 +1,11 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
-# Deploy with `firebase deploy`
-
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
 
+# initialize
 initialize_app()
 
+# define function endpoint
 @https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+def baraudio(req: https_fn.Request) -> https_fn.Response:
+    response = "Python is better than JavaScript"
+    return https_fn.Response(response)
