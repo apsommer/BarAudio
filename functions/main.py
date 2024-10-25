41a0dc719fd625d5b3ea0a5fcfabfab700062bcc
Drew Sommer <drew@sommerengineering.com>
2024-10-25 13:08:04 +0300

    feat: Parse GET and POST requests

--- functions/main.py
+++ functions/main.py
@@ -1,14 +1,28 @@
 from firebase_functions import https_fn
-from firebase_admin import initialize_app
-from flask import request, json
+# from firebase_admin import initialize_app
+from flask import Flask, json, request
 
 # initialize
-initialize_app()
+# initialize_app()
+
+# initialize
+app = Flask(__name__)
 
 # define function endpoint
-@https_fn.on_request()
-def baraudio(request: https_fn.Request) -> https_fn.Response:
-    requestJson = request.json
-    jsonString = json.dumps(requestJson, default = str)
-    response = jsonString
-    return https_fn.Response(response)
+# @https_fn.on_request()
+
+# define function endpoint
+@app.route("/", methods=["GET", "POST"])
+# def baraudio(req: https_fn.Request) -> https_fn.Response:
+def baraudio() -> https_fn.Response:
+
+    response_message = ""
+
+    if request.method == "GET":
+        response_message = "GET request received"
+
+    if request.method == "POST":
+        response_message = "POST request received"
+
+    response = https_fn.Response(response_message)
+    return response
