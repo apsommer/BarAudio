1d32b74544e6e2aa7de7b7fb864f9f78a22299a4
Drew Sommer <drew@sommerengineering.com>
2024-11-17 18:51:36 +0200

    feat: Toggle origin icon

--- app/src/main/java/com/sommerengineering/baraudio/messages/Message.kt
+++ app/src/main/java/com/sommerengineering/baraudio/messages/Message.kt
@@ -1,6 +1,28 @@
 package com.sommerengineering.baraudio.messages
 
+import com.sommerengineering.baraudio.R
+
 data class Message(
     var timestamp: String,
-    var message: String
+    var message: String,
+    var originImageId: Int?
 )
+
+fun getOriginImageId(origin: String): Int? {
+
+    if (origin.contains("insomnia")) return R.drawable.insomnia
+    tradingviewWhitelistIps.forEach { if (origin.equals(it)) return R.drawable.tradingview }
+    if (origin.equals(trendspiderWhitelistIp)) return R.drawable.trendspider
+
+    return null
+}
+
+// https://www.tradingview.com/support/solutions/43000529348-about-webhooks/
+val tradingviewWhitelistIps = listOf(
+    "52.89.214.238",
+    "34.212.75.30",
+    "54.218.53.128",
+    "52.32.178.7")
+
+// https://help.trendspider.com/kb/alerts/webhooks
+val trendspiderWhitelistIp = "3.12.143.24"
