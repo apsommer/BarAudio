c18c027c0858fe009cf54305f2e291ed49312864
Drew Sommer <drew@sommerengineering.com>
2024-11-20 22:30:34 +0200

    refactor: Remove unused code

--- app/src/main/java/com/sommerengineering/baraudio/login/LoginScreen.kt
+++ app/src/main/java/com/sommerengineering/baraudio/login/LoginScreen.kt
@@ -1,6 +1,5 @@
 package com.sommerengineering.baraudio.login
 
-import android.widget.ImageButton
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
