8a92f9cbccc61879995f14d85398c15918a2dc73
Drew Sommer <drew@sommerengineering.com>
2024-11-23 22:40:05 +0200

    feat: Stub floating action button

--- app/src/main/java/com/sommerengineering/baraudio/messages/MessagesScreen.kt
+++ app/src/main/java/com/sommerengineering/baraudio/messages/MessagesScreen.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -81,7 +84,19 @@ fun MessagesScreen(
             MessagesTopBar(
                 onSettingsClick,
                 messages)
-        }) { scaffoldPadding ->
+        },
+        floatingActionButton = {
+            LargeFloatingActionButton (
+                shape = CircleShape,
+                onClick = { logMessage("Click on fab") }
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.webhook),
+                    contentDescription = null
+                )
+            }
+        }
+    ) { scaffoldPadding ->
 
         Box(Modifier
             .fillMaxSize()
