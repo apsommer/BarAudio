4f1420fc21752d055c3a23a997b025b1b8bbae73
Drew Sommer <drew@sommerengineering.com>
2024-11-27 22:14:58 +0200

    fix: Increase setting button touch target

--- app/src/main/java/com/sommerengineering/baraudio/settings/DialogSettingItem.kt
+++ app/src/main/java/com/sommerengineering/baraudio/settings/DialogSettingItem.kt
@@ -2,7 +2,9 @@ package com.sommerengineering.baraudio.settings
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,6 +13,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -29,15 +33,13 @@ fun DialogSettingItem(
     @DrawableRes icon: Int,
     @StringRes title: Int,
     description: String,
-    onClick: () -> Unit,
     content: @Composable () -> Unit) {
 
     Surface {
         Column {
             Row(
                 modifier = Modifier
-                    .clickable { onClick() }
-                    .padding(end = 24.dp),
+                    .padding(end = 12.dp),
                 verticalAlignment = Alignment.CenterVertically) {
 
                 Row(
@@ -47,15 +49,10 @@ fun DialogSettingItem(
 
                     Box(
                         modifier = Modifier
-                            .clickable { onClick() }
                             .padding(24.dp)) {
                         Icon(
                             modifier = Modifier
-                                .size(24.dp)
-                                .clickable {
-                                    onClick()
-                                           logMessage("apples")
-                                           },
+                                .size(24.dp),
                             painter = painterResource(icon),
                             contentDescription = null)
                     }
