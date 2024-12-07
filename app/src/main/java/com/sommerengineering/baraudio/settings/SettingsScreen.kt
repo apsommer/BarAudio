e58194929ef7278f00208b62d9abed8da3b71519
Drew Sommer <drew@sommerengineering.com>
2024-12-07 15:48:35 +0200

    feat: Add version code to settings screen

--- app/src/main/java/com/sommerengineering/baraudio/settings/SettingsScreen.kt
+++ app/src/main/java/com/sommerengineering/baraudio/settings/SettingsScreen.kt
@@ -1,22 +1,30 @@
 package com.sommerengineering.baraudio.settings
 
 import android.content.Intent
-import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.sommerengineering.baraudio.MainActivity
 import com.sommerengineering.baraudio.MainViewModel
 import com.sommerengineering.baraudio.R
@@ -196,6 +204,17 @@ fun SettingsScreen(
                     icon = R.drawable.sign_out,
                     title = R.string.sign_out,
                     onClick = { onSignOut() })
+            }
+
+            // version code
+            item {
+                Text(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 24.dp, end = 24.dp, bottom = 24.dp),
+                    text = "2.0.071224", // todo get from build config
+                    textAlign = TextAlign.End,
+                    style = MaterialTheme.typography.bodySmall)
             }
         }
     }
