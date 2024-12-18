7a135096dad5d756276eaeccf5b2cf33370b25aa
Drew Sommer <drew@sommerengineering.com>
2024-12-18 19:34:44 +0200

    style: Animate color of delete all button

--- app/src/main/java/com/sommerengineering/baraudio/messages/MessagesTopBar.kt
+++ app/src/main/java/com/sommerengineering/baraudio/messages/MessagesTopBar.kt
@@ -1,26 +1,39 @@
 package com.sommerengineering.baraudio.messages
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.sommerengineering.baraudio.R
+import com.sommerengineering.baraudio.theme.uiModeFadeTimeMillis
 
-// todo do this without experimental optin
+// todo do this without experimental optin?
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MessagesTopBar(
     onSettingsClick: () -> Unit,
-    messages: SnapshotStateList<Message>) {
+    messages: SnapshotStateList<Message>
+) {
 
-    return CenterAlignedTopAppBar(
+    // animate color of "delete all" button
+    var isEmpty by remember { mutableStateOf(true) }
+    isEmpty = messages.isEmpty()
+
+    CenterAlignedTopAppBar(
 
         modifier = Modifier
             .padding(8.dp),
@@ -53,6 +66,13 @@ fun MessagesTopBar(
                 enabled = !messages.isEmpty()) {
                 Icon(
                     painter = painterResource(R.drawable.done_all),
+                    tint = animateColorAsState(
+                        targetValue =
+                            if (isEmpty) IconButtonDefaults.iconButtonColors().disabledContentColor
+                            else IconButtonDefaults.iconButtonColors().contentColor,
+                        animationSpec = tween(uiModeFadeTimeMillis),
+                        label = ""
+                    ).value,
                     contentDescription = null)
             }
         })
