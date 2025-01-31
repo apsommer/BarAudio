0384d9666445ec55574684b37b22a9f71cf79d20
Drew Sommer <drew@sommerengineering.com>
2025-01-31 23:29:17 +0100

    refactor: Simplify first onboarding screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ import java.io.FileInputStream
 import java.util.Properties
 
 plugins {
-
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.services)
