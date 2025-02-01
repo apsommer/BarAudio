4c25836894f77295d260353ad674be33e862ecf3
Drew Sommer <drew@sommerengineering.com>
2025-02-01 18:54:59 +0100

    fix: Navigation crash on orientation change

--- app/src/main/java/com/sommerengineering/baraudio/Navigation.kt
+++ app/src/main/java/com/sommerengineering/baraudio/Navigation.kt
@@ -7,7 +7,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
