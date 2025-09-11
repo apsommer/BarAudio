d6aba78a1f40835b0ab3285d51db3c149facb8e8
Drew Sommer <drew@sommerengineering.com>
2025-09-11 12:37:22 +0200

    version: Upgrade java compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ keystoreProperties.load(
 
 kotlin {
     compilerOptions {
-        jvmTarget = JvmTarget.fromTarget("17")
+        jvmTarget = JvmTarget.fromTarget("21")
         freeCompilerArgs = listOf("-Xdebug") // enable all variables in debugger break points
     }
 }
@@ -83,8 +83,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
