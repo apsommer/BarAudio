f6c1c91deabdce1cf304cf7447e6f3a35abca35b
Drew Sommer <drew@sommerengineering.com>
2026-01-05 16:46:16 +0100

    feat: Design divider for settings ui

---
+++ app/src/main/java/com/sommerengineering/baraudio/settings/DividerItem.kt
@@ -0,0 +1,41 @@
+package com.sommerengineering.baraudio.settings
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sommerengineering.baraudio.edgePadding
+
+@Composable
+fun DividerItem(
+    text: String) {
+
+    Surface {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(edgePadding)) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(start = 0.dp, end = edgePadding, top = 0.dp, bottom = 0.dp)
+            )
+            HorizontalDivider()
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun DividerItemPreview() {
+    DividerItem("Divider")
+}
--- app/src/main/java/com/sommerengineering/baraudio/settings/LinkItem.kt
+++ app/src/main/java/com/sommerengineering/baraudio/settings/LinkItem.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import com.sommerengineering.baraudio.edgePadding
 import com.sommerengineering.baraudio.settingsIconSize
 
