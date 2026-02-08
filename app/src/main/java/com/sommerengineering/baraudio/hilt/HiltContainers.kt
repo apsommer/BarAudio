08bb7d2451a62f2a10e7387b2e8b7f6cd3088838
Drew Sommer <drew@sommerengineering.com>
2026-02-08 20:08:03 +0100

    perf: Create injectable application scope

--- app/src/main/java/com/sommerengineering/baraudio/hilt/HiltContainers.kt
+++ app/src/main/java/com/sommerengineering/baraudio/hilt/HiltContainers.kt
@@ -7,7 +7,15 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ApplicationScope
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -32,6 +40,13 @@ object ApplicationModule {
     fun provideBillingClient(
         @ApplicationContext context: Context): BillingClientImpl {
         return BillingClientImpl(context)
+    }
+
+    @Provides
+    @Singleton
+    @ApplicationScope
+    fun provideApplicationScope(): CoroutineScope {
+        return CoroutineScope(SupervisorJob() + Dispatchers.IO)
     }
 }
 
