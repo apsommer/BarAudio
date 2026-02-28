c64dfe8a9947aec944552cfee4ff8a0bf891cb19
Drew Sommer <drew@sommerengineering.com>
2026-02-28 01:10:53 +0100

    style: Scrim over background image

--- app/src/main/java/com/sommerengineering/baraudio/messages/MessagesScreen.kt
+++ app/src/main/java/com/sommerengineering/baraudio/messages/MessagesScreen.kt
@@ -3,6 +3,7 @@ package com.sommerengineering.baraudio.messages
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +12,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.DrawerDefaults
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
@@ -23,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -75,6 +78,9 @@ fun MessagesScreen(
 
             Box(Modifier.fillMaxSize().padding(padding)) {
 
+                // todo pull background and scrim to separate composable
+                //  apply same scrim over app bar logo
+
                 // background image
                 Image(
                     painter = painterResource(backgroundImageId),
@@ -83,6 +89,14 @@ fun MessagesScreen(
                     modifier = Modifier
                         .align(Alignment.Center)
                         .padding(2 * fabPadding))
+
+                // scrim overlay
+                Box(
+                    modifier = Modifier
+                        .matchParentSize()
+                        .background(
+                            if (isDarkMode) MaterialTheme.colorScheme.background.copy(alpha = 0.6f)
+                            else MaterialTheme.colorScheme.background.copy(alpha = 0.8f)))
 
                 // messages
                 LazyColumn(state = listState) { when (feedMode) {
