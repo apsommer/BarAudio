0a9edf275e95aeb728fb487eeace61a21ea9fc8a
Drew Sommer <drew@sommerengineering.com>
2026-03-18 12:05:38 +0000

    docs: Add comments

--- app/src/main/java/com/sommerengineering/baraudio/uitls/Constants.kt
+++ app/src/main/java/com/sommerengineering/baraudio/uitls/Constants.kt
@@ -17,8 +17,9 @@ fun logException(e: Exception) {
 
 // durations
 const val bottomBarTransitionTimeMillis = 1000
-const val colorTransitionTimeMillis = 100
+const val colorTransitionTimeMillis = 0
 const val messageItemExpansionTimeMillis = 140
+// todo put screen fades here, reduce them to 10% of current
 
 // urls
 const val setupWebhookUrl = "https://sommerengineering.com/baraud.io"
