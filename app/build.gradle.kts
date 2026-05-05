d087dd4e731546bf55b6276499a596fe346187c1
Drew Sommer <drew@sommerengineering.com>
2026-05-05 23:24:08 +0100

    release: v1.7 (8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ configure<ApplicationExtension> {
         applicationId = "com.sommerengineering.signalvoice"
         minSdk = 28
         targetSdk = 36
-        versionCode = 6 // increment for each release
-        versionName = "1.5" // major.minor.date.letter
+        versionCode = 8 // increment for each release
+        versionName = "1.7" // major.minor.date.letter
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
